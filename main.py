7792564419a0f00264a77a673465fa4ae2adfbb4
RykerK-UARK <rykerk@uark.edu>
2026-09-16 18:22:26 -0500

    UDP Basic Setup & Testing

--- main.py
+++ main.py
@@ -3,5 +3,36 @@
 # Library imports
 import socket
 
-### Reserved space for socket stuff
-###
+### Socket Section Start ###
+
+# Setup broadcast socket
+broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcast.connect(("127.0.0.1", 7500))	
+
+# Setup receiving socket
+receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receive.bind(("0.0.0.0", 7501))
+receive.setblocking(False)
+
+## Test code, designed to work with test_server.py 
+# num = 0
+# while True:
+# 	# Send current number
+#	broadcast.send(str(num).encode("utf-8"))
+#	print(f"{num} sent")
+#	
+#	# Receive server response from other socket
+#	data, address = receive.recvfrom(1024)
+#	num = int(str(data, "utf-8"))
+#	print(f"{num} received")
+#	
+#	# If the received number is greater than 5, end the process
+#	if num > 5:
+#		print("Process terminated")
+#		break
+
+# Close sockets
+broadcast.close()
+receive.close()
+
+### Socket Section End ###
---
+++ test_server.py
@@ -0,0 +1,40 @@
+# This is a test file used to simulate a server for testing UDP
+# sockets sending and receiving information.
+
+# Library imports
+import socket
+
+# Setup broadcast socket
+broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcast.connect(("127.0.0.1", 7501))	
+
+# Setup receiving socket
+receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receive.bind(("0.0.0.0", 7500)) # Should listen to UDP packets from any address at port 7501
+
+while True:
+	# Get data in bytes
+	b_data, address = receive.recvfrom(1024)
+	
+	# Convert data received to string
+	data = str(b_data, "utf-8")
+	
+	# Checks to make sure the data received is an integer
+	try:
+		int(data)
+	except:
+		print("ERROR: Data received is not an integer!")
+		continue
+	
+	# Store data as an integer
+	i_data = int(data)
+		
+	# Use the other socket to send the previous data + 1
+	broadcast.send(str(i_data + 1).encode("utf-8"))
+	
+	if i_data == 100:
+		break
+
+# Close sockets
+broadcast.close()
+receive.close()
